01c98e8fd34f74e7f8a4b1705707f7ba81e1f908
841140915@qq.com <841140915@qq.com>
2018-06-06 16:10:02 +0800

    cheng le!

--- src/FirstDemo.java
+++ src/FirstDemo.java
@@ -7,7 +7,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 /**
  * @Auther: Lee
  * @Date: 2018/6/6 08:42
- * @Description:
+ * @Description: 每次有web连接就会新建一个FirstDemo对象
  */
 
 @ServerEndpoint(value = "/entry")
@@ -15,11 +15,12 @@ public class FirstDemo {
     public FirstDemo() {
         System.out.println("已经初始化了了乐乐了乐乐");
     }
-
+    private String name ;
     private static Integer online = 0;
     private static CopyOnWriteArraySet<FirstDemo> copy = new CopyOnWriteArraySet<>();
     private Session session;
 
+    //建立socket连接
     @OnOpen
     public void demoOpen(Session session){
         this.session = session;
@@ -27,40 +28,47 @@ public class FirstDemo {
         addOnline();
     }
 
-    private synchronized void addOnline() {
-        FirstDemo.online++;
-        System.out.println("当前在线人数为："+FirstDemo.online);
-    }
+
+    //接收信息
     @OnMessage
     public void demoSendMessage(String message,Session session){
-        sendMessage(message);
+        //接到信息后首先返回信息
+        sendMessage("你好我收到了你的hello！当前在线："+online+"人");
         System.out.println("客户端发送消息："+message);
         System.out.println("websocket数量："+copy.size());
-        for (FirstDemo fd:copy){
-            fd.sendMessage("服务端发来的消息！");
-        }
+//        for (FirstDemo fd:copy){
+//            fd.sendMessage("服务端发来的消息！");
+//        }
 
     }
 
     private void sendMessage(String message) {
         try {
+            //发送客户端信息
             this.session.getBasicRemote().sendText(message);
             System.out.println("========================发送了信息："+message);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    //socket关闭后将对象移除
     @OnClose
     public void demoClose(){
         copy.remove(this);
         offOnLine();
     }
 
+    //建立连接后增加
+    private synchronized void addOnline() {
+        FirstDemo.online++;
+        System.out.println("当前在线人数为："+FirstDemo.online);
+    }
+    //断开连接后减去
     private synchronized void offOnLine() {
         FirstDemo.online--;
         System.out.println("当前在线人数为："+FirstDemo.online);
     }
+
     @OnError
     public void demoError(Session session, Throwable throwable) throws SocketException {
         throw  new SocketException(throwable.getLocalizedMessage());
